0922126044306bd9edb208fcc96d80416b824fef
Sebastian Tamayo <sebastian-tamayo.pacheco@brightstraining.com>
2023-07-05 19:37:41 +0200

    first commit

---
+++ api-tt/src/main/java/seta1609co/apitt/ApiTtApplication.java
@@ -0,0 +1,13 @@
+package seta1609co.apitt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTtApplication.class, args);
+	}
+
+}
---
+++ api-tt/src/test/java/seta1609co/apitt/ApiTtApplicationTests.java
@@ -0,0 +1,13 @@
+package seta1609co.apitt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
